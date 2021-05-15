20c35161b8bcf8899ec94b248bfac6bbe0cde92f
accetto <34798830+accetto@users.noreply.github.com>
2021-05-15 20:47:55 +0200

    script 'releas_of.sh' improved

--- docker/hooks/release_of.sh
+++ docker/hooks/release_of.sh
@@ -5,27 +5,33 @@ main() {
 
     case "$1" in
 
-        chromium-1804)
-            result=$( \
-                curl -s http://archive.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/ \
+        chromium-1804 )
+            # result=$(curl -sL \
+            result=$(wget -qO- \
+                http://archive.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/ \
                 | grep -Po -m1 '(?<=href=")[^_]*_([0-9.]+-0ubuntu0\.18\.04\.[^_"]*)_[^"]*' \
-                | cut -d _ -f 2 )
-            ;;
-
-        nodejs-lts )
-            result=$( \
-                curl -s https://nodejs.org/en/download/ \
-                | grep "Latest LTS Version:" \
-                | grep -Po -m1 "[0-9.]+" \
-                | head -n1 )
+                | cut -d _ -f 2 \
+            )
             ;;
 
         nodejs-current )
-            result=$( \
-                curl -s https://nodejs.org/en/download/current/ \
+                # curl -sL https://nodejs.org/en/download/current/ \
+            result=$(wget -qO- \
+                https://nodejs.org/en/download/current/ \
                 | grep "Latest Current Version:" \
-                | grep -Po -m1 "[0-9.]+" \
-                | head -n1 )
+                | grep -Po -m1 '[0-9.]+' \
+                | head -n1
+            )
+            ;;
+
+        nodejs-lts )
+                # curl -sL https://nodejs.org/en/download/ \
+            result=$(wget -qO- \
+                https://nodejs.org/en/download/ \
+                | grep 'Latest LTS Version:' \
+                | grep -Po -m1 '[0-9.]+' \
+                | head -n1
+            )
             ;;
 
         * )
